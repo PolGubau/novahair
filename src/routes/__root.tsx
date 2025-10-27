7756cbc8d68090cb11953707b012fed48b242b7a
Pol Gubau Amores <gubaupol@gmail.com>
2025-10-27 21:28:03 +0100

    Add Vite client reference and reorder import statements
    This commit introduces a reference to the Vite client in the __root.tsx file. Additionally, the import statement for the LandingPage component in index.tsx has been reordered for better clarity and organization. These changes enhance the overall structure of the codebase.
    
    Issue: MW-0000
    Type: chore

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,3 +1,4 @@
+/// <reference types="vite/client" />
 import { TanStackDevtools } from "@tanstack/react-devtools";
 import type { QueryClient } from "@tanstack/react-query";
 import {
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,5 +1,5 @@
-import { LandingPage } from "@/features/landing/landing-page";
 import { createFileRoute } from "@tanstack/react-router";
+import { LandingPage } from "@/features/landing/landing-page";
 
 export const Route = createFileRoute("/")({
 	component: App,
