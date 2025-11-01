c1a26350d86a6d9310023ba042df89cc47b8dcd3
Pol Gubau Amores <gubaupol@gmail.com>
2025-11-01 17:51:49 +0100

    Add minimum height to services section for better layout
    This change ensures that the services section maintains a minimum height of 250px, improving the overall layout and visual consistency of the page. This adjustment enhances user experience by preventing the section from collapsing too much on smaller screens.
    
    Issue: MW-0000
    Type: style

--- src/features/landing/ui/services.tsx
+++ src/features/landing/ui/services.tsx
@@ -5,7 +5,7 @@ import { ServiceList } from "~/features/appointment-form/ui/services/list";
 export const Services = () => {
 	const { services } = useServices();
 	return (
-		<section className="flex flex-col items-center gap-4 max-w-7xl w-full mx-auto px-4 min-h-[85vh] py-12">
+		<section className="flex flex-col items-center gap-4 max-w-7xl w-full mx-auto px-4 min-h-[65vh] py-12">
 			<h2 className="text-4xl lg:text-6xl">{t("our_services")}</h2>
 			<p className="text-center text-pretty max-w-md mb-16">
 				{t("description_services")}
